6ca37c6f295107e172ab42a3504d4cf87e742759
alimadan196 <alimadan196@gmail.com>
2025-01-24 15:40:50 +0300

    Added components and news pages

--- app/news/[id]/page.js
+++
@@ -1,9 +0,0 @@
-export default function NewsDetailPage({ params }) {
-    const id = params.id
-
-    return
-    <>
-        <h1>News Detail Page</h1>
-        <p>News ID</p>
-    </>
-}
--- app/news/page.js
+++ app/news/page.js
@@ -1,20 +1,11 @@
-import Link from "next/link";
+import { DUMMY_NEWS } from "@/dummy-news";
+import NewsList from "@/components/news-list";
 
 export default function NewsPage() {
     return (
         <>
             <h1>News Page</h1>
-            <ul>
-                <li>
-                    <Link href="/news/first-news">First News Item</Link>
-                </li>
-                <li>
-                    <Link href="/news/second-news">Second News Item</Link>
-                </li>
-                <li>
-                    <Link href="/news/third-news">Third News Item</Link>
-                </li>
-            </ul>
+            <NewsList news={DUMMY_NEWS} />
         </>
     )
 
