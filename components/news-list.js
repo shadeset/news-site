c8b9b3eaebbeecfada3de589b5f349681773ae30
alimadan196 <alimadan196@gmail.com>
2025-01-25 12:47:48 +0300

    Added a catch-all route for filtered articles on @archive and added a fallback error page for invalid/inexistent filters

--- components/news-list.js
+++ components/news-list.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function NewsList({ news }) {
     return <ul className="news-list">
-        {DUMMY_NEWS.map(newsItem => (
+        {news.map(newsItem => (
             <li key={newsItem.id}>
                 <Link href={`/news/${newsItem.slug}`}>
                     <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
